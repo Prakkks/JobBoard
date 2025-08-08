bea71588c158b6e9e724a41ce61ab85ac0fd5f6a
Prakkks <tprakrity@gmail.com>
2025-08-08 12:35:36 +0545

    Added functionality to filter job based on Location and JobType

--- src/pages/JobDetailPage.tsx
+++ src/pages/JobDetailPage.tsx
@@ -2,21 +2,43 @@ import axios from "axios";
 import { useEffect, useState } from "react"
 import type { JobListValues } from "../constants/constant";
 import ListedJobCard from "../Components/ListedJobCard";
-import FilterJob from "../Components/FilterJob";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form"
+import { JobFilterSchema, type JobFilterSchemaType } from "../Validation/validate";
+
+
+
 
 
 const JobDetailPage = () => {
   
   const [job, setJob] = useState<JobListValues[]>([]);
+  const [reloadJobShow , setReloadJobShow] = useState<JobFilterSchemaType>({'jobType':'' ,'location':'' });
   const baseURL = import.meta.env.VITE_BASE_URL
   
-
+  const {register , formState: {errors}, handleSubmit} = useForm( {resolver: zodResolver(JobFilterSchema)} );
+    const onSubmit = (data:JobFilterSchemaType)=> {
+        console.log(data);
+        setReloadJobShow(data);
+    }
 
   useEffect(
 
     ()=> {
+
+      let url = baseURL+'/api/job/getAllJobs';
       
-      axios.get(baseURL+'/api/job/getAllJobs')
+      if (reloadJobShow.jobType !== '' && reloadJobShow.location !== '') {
+        url += `?jobType=${reloadJobShow.jobType}&location=${reloadJobShow.location}`; 
+      }
+      else if (reloadJobShow.jobType !== '' ){
+        url += `?jobType=${reloadJobShow.jobType}`;
+      }
+      else if (reloadJobShow.location !== ''){
+        url += `?location=${reloadJobShow.location}`;
+      }
+      console.log('url = ', url);
+      axios.get(url)
       .then((response)=> {
           setJob(response.data.jobs);
           const arr = response.data.jobs;
@@ -30,9 +52,9 @@ const JobDetailPage = () => {
      
 
       
-
+     
     },
-    []);
+    [reloadJobShow]);
 
 
 
@@ -40,14 +62,36 @@ const JobDetailPage = () => {
 
   return (
     
+    
     <section className="sections flex flex-col gap-4  ">
         
-      <FilterJob />  
-       <div  className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-4 gap-y-7 justify-evenly w-full">
        
+       <div className="flex flex-row sm:pb-5  p-2 shadow-sm shadow-gray-100   ">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col sm:flex-row items-center justify-around w-full " > 
+           
+           <label className="flex flex-col flex-1/4 bg-white "> <p className="text-sm">TYPE</p>  
+           <select {...register('jobType')} className="border-none  bg-[#f6f3f4] w-1/2 rounded-md px-4 py-2  outline-none" >
+                <option className="" value={''}> Select </option>
+                <option value={'Contract'}> Contract </option>
+                <option value={'Part-time'}> Part Time</option>
+                <option value={'Full-time'}> Full Time</option>
+                <option value={'Internship'}> Internship</option>
+            </select>
+            {errors.jobType && <div>{errors.jobType.message}</div>}
+            </label>
+            <label className="flex flex-col flex-1/4 "><p className="text-sm" > CATEGORY</p> <input placeholder="Eg. Kathmandu"  type="text" {...register('location')} className=" border-[#f6f3f4]  bg-[#f6f3f4] w-1/2 rounded-md px-4 py-2 " />
+            {errors.location && <div>{errors.location.message}</div>}
+            </label>
+            <span className="flex-1/4  flex-row  justify-center w-full "> <button className=" cursor-pointer  rounded-md w-1/2 justify-center border-2 hover:bg-[#f6f3f4] border-[#f6f3f4] px-4 py-2 flex flex-row gap-2 items-center font-semibold text-black"> <img src="/Icons/search.png" className="w-5 h-5" /> Search</button></span>
+ 
+        </form>
+
+    </div>
 
 
 
+       <div  className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-x-4 gap-y-7 justify-evenly w-full">
+    
        {job.length === 0 ? (
         <p>No result found..</p>
       ) : (
